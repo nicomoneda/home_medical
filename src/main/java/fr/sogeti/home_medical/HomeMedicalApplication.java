cc8312e4dc8cf6d642d8acae2326141a698964e1
A Sathiesh <satwebfr@gmail.com>
2022-09-06 16:40:21 +0200

    Initial Commit

---
+++ src/main/java/fr/sogeti/home_medical/HomeMedicalApplication.java
@@ -0,0 +1,13 @@
+package fr.sogeti.home_medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeMedicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeMedicalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/sogeti/home_medical/HomeMedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sogeti.home_medical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeMedicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
